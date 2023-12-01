fb9c336f725a99030f97600148d4a9c6ddf6a5b0
Suleman <sulaiman.amer@gmail.com>
2023-12-02 01:19:24 +0500

    dashboard app

--- diabetesDetection3/settings.py
+++ diabetesDetection3/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'patientInfo',
     'adminInfo',
     'resources',
     'notifications',
     'socialMedia',
-    'django_extensions',
+    'dashboard'
+    
 ]
 
 MIDDLEWARE = [
